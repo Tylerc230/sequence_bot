0983f8d33a0a898e712b591957fc90330f88153b
Tyler Casselman <tylerc230@gmail.com>
2012-05-07 21:08:20 -0700

    Read a simple webpage from flash and returning it on GET request
    We are storing a simple web page in flash memory and retrieving
    it for every GET request to arduino

--- src/MyWebServer.cpp
+++ src/MyWebServer.cpp
@@ -1,11 +1,13 @@
 #include "MyWebServer.h"
 #include "Arduino.h"
+#include <avr/pgmspace.h>
 #define APP_WISERVER
 #include "WiServer.h"
 extern "C"
 {
 #include "MyConfig.h"
 }
+const prog_char html[] PROGMEM  = {"<html> <body> <h1>My First Heading</h1> <p>My first paragraph.</p> </body> </html> "};
 boolean response(char * url);
 
 MyWebServer::MyWebServer(Router * router)
@@ -23,8 +25,7 @@ void MyWebServer::run()
 
 boolean response(char * url)
 {
-	Serial.println("here");
-	WiServer.println_P("<html> <body> <h1>My First Heading</h1> <p>My first paragraph.</p> </body> </html> ");
+	WiServer.println_P(html);
 }
 
 
