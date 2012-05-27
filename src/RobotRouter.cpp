40a321bd99fa414a7ac50955de4cc82ca780a683
Tyler Casselman <tylerc230@gmail.com>
2012-05-27 13:08:48 -0700

    Creating resource declarations and resource mappings file
    These 2 files will be used to declare pointers to resources (images,
    html files) in flash memory, and mappings of path strings to those
    pointers respectively ('image/my_image.png' => image_my_image_png)

--- src/RobotRouter.cpp
+++ src/RobotRouter.cpp
@@ -1,18 +1,9 @@
 #include "RobotRouter.h"
 #include "WiServer.h"
 #include "Arduino.h"
-#include "resources.h"
-
-void controls_ui(char * url);
 
 void RobotRouter::initRoutes()
 {
 	Router::initRoutes();
-	Serial.println("derived initRoutes");
-	this->addRoute("/index.html", controls_ui);
 }
 
-void controls_ui(char * url)
-{
-	WiServer.println_P(resources_controls_html);
-}
